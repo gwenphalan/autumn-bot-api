741f5f888c3ff300a7130f81086c3ce620a9f57c
Gwen <creepthewolf@icloud.com>
2020-05-23 19:39:34 -0400

    fix host

--- src/config/development.ts
+++ src/config/development.ts
@@ -4,7 +4,7 @@ export default {
   token: "NjczNDI0MTkzODYxMTg5NjYx.XsiPOQ.DKYIPa9-7i9lXp473-oj_s1LIl8",
   clientID: "673424193861189661",
   clientSecret: "05BUUrB7-0SJiz1H7NbBVt3sWR3Wo7mR",
-  host: "localhost:3000",
+  host: "api.autumnbot.net",
   website: "localhost:4200",
   mongoString:
     "mongodb+srv://autumn_beta:d3OmzlQRB5zTaBmu@cluster0-9pmgv.mongodb.net/test?retryWrites=true&w=majority",
