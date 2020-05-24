c96721dff9af5c663889e7c8e607cc14f7ec0c59
Gwen <creepthewolf@icloud.com>
2020-05-23 20:20:57 -0400

    Add Headers

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,24 @@ import { config } from "./config";
 
 const app = express();
 
+app.use((_req, res, next) => {
+  res.setHeader("X-Powered-By", "Weebs");
+
+  res.setHeader("X-Content-Type-Options", "nosniff");
+  res.setHeader("X-Xss-Protection", "1; mode=block");
+  res.setHeader("X-Frame-Options", "SAMEORIGIN");
+  res.setHeader(
+    "Strict-Transport-Security",
+    "max-age=31536000; includeSubdomains"
+  );
+  res.setHeader("Content-Security-Policy", "default-src https:");
+  res.setHeader("Referrer-Policy", "no-referrer");
+
+  next();
+});
+
+app.enable("trust proxy");
+
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors());
