4214001ae620901fc2101cf0d93ab8bb3053f86b
GwenBebe <creepthewolf@icloud.com>
2020-08-15 18:57:41 -0500

    Setting Up for Docker

--- src/api/controllers/discord.ts
+++ src/api/controllers/discord.ts
@@ -253,7 +253,7 @@ export const updateGuild = async (req: Request, res: Response) => {
 
   const client = new net.Socket();
 
-  client.connect({ port: 8124, host: "0.0.0.0" }, () => {
+  client.connect({ port: 8124, host: "localhost" }, () => {
     console.log("Connected to bot.");
 
     const update = {
